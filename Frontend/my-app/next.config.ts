32813bde9ec843337663b51297b8179481196cce
zvexl <2300945@sit.singaporetech.edu.sg>
2024-10-29 23:56:06 +0800

    Implement hot reloading

--- Frontend/my-app/next.config.ts
+++ Frontend/my-app/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpackDevMiddleware: (config: Configuration) => {
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // Delay before rebuilding
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
